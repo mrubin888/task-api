b17d9d04b990ffddbe330eb5e374ecfbe2d97af0
matt rubin <matt@vital.enterprises>
2018-10-08 13:52:45 -0700

    project setup

---
+++ resources/tasks.py
@@ -0,0 +1,16 @@
+# tasks.py
+
+# Let's get this party started!
+import falcon
+
+# Falcon follows the REST architectural style, meaning (among
+# other things) that you think in terms of resources and state
+# transitions, which map to HTTP verbs.
+class TasksResource(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = ('\nTwo things awe me most, the starry sky '
+                     'above me and the moral law within me.\n'
+                     '\n'
+                     '    ~ Immanuel Kant\n\n')
